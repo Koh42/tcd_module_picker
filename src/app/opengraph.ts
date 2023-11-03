bc8d76ba86ee974a78b58a8152fa8e2f136259b5
SM.K <sweemeng.koh@gmail.com>
2023-11-03 17:55:29 +0800

    fix opengraph newline character

--- src/app/opengraph.ts
+++ src/app/opengraph.ts
@@ -1,18 +1,18 @@
 import type { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types'
 
 export const openGraph: OpenGraph = {
-  title: 'ft_transcendence(v14) Module Picker',
-  description: 'ft_transcendence Module Picker',
+  title: 'ft_transcendence Module Picker',
+  // description: 'ft_transcendence Module Picker',
   url: 'https://tcdmodules.vercel.app',
   siteName: 'Next.js',
   images: [
     {
-      url: 'https://placehold.co/600x315/tomato/white/png?text=ft_transcendence\nModule+Picker&font=montserrat',
+      url: 'https://placehold.co/600x315/tomato/white/png?text=ft_transcendence%0AModule+Picker&font=montserrat',
       width:  600,
       height: 315,
     },
     {
-      url: 'https://placehold.co/1200x630/tomato/white/png?text=ft_transcendence\nModule+Picker&font=montserrat',
+      url: 'https://placehold.co/1200x630/tomato/white/png?text=ft_transcendence%0AModule+Picker&font=montserrat',
       width: 1200,
       height: 630,
     },
